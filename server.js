539b94a409ce9671216154e762e3cd36da2dee7e
Patrick Smith <patrick@burntcaramel.com>
2018-03-06 23:27:22 +1100

    Add NODE_ENV check for loading .env

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Hapi = require('hapi')
 const Octokit = require('@octokit/rest')
 const axios = require('axios')
